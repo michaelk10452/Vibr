03f58a31549d8af44ababb7fb3763455333e230d
Paul <pgn03@mail.aub.edu>
2023-02-05 07:05:49 -0800

    Final

--- main.js
+++ main.js
@@ -2,23 +2,41 @@ const app = new Vue({
     el: '#app',
     data: {
       responseData: null,
-      file: null
+      file: null,
+      isRequesting: false
     },
     methods: {
+
       uploadFile(event) {
+      
         this.file = event.target.files[0];
+        this.responseData = null;
+      
       },
+      
       async submitFile() {
+      
         if (!this.file) {
           alert("Please upload a file before submitting");
           return;
         }
+      
         const formData = new FormData();
+      
         formData.append('file', this.file);
+      
+        this.isRequesting = true;
+
         const { data } = await axios.post('http://127.0.0.1:5000/upload', formData);
+      
+        this.isRequesting = false;
+
         console.log(typeof data);
+      
         this.responseData = data;
+      
         console.log(this.responseData)
+      
       }
       
     }
